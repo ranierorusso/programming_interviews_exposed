6f495e219faf55a89879498125e071049aac29ec
Raniero Russo <ranierorusso@gmail.com>
2015-11-23 15:47:28 +0000

    implementation for bubble sort method

--- lib/array_sort.rb
+++ lib/array_sort.rb
@@ -1,4 +1,25 @@
 class ArraySort
+
   def self.bubble_sort(arr, order: :asc)
+    raise TypeError if arr.nil?
+    raise TypeError unless arr.all? {|i| i.is_a? Integer }
+    return arr if arr.empty?
+
+    comparator = (order == :asc ? :< : :>)
+
+    loop do
+      swapped = false
+
+      for i in 0...arr.length-1
+        if (arr[i+1].send(comparator, arr[i]))
+          arr[i+1], arr[i] = arr[i], arr[i+1]
+          swapped = true
+        end
+      end
+
+      break if swapped == false
+    end
+
+    arr
   end
 end
